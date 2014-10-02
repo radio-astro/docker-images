030f24bab6a9783b20f16970836080d7ced4ef2f
Gijs Molenaar <gijs@pythonic.nl>
2014-10-02 13:57:20 +0200

    adjust vagrant to new layout

--- vagrant/provision.sh
+++ vagrant/provision.sh
@@ -3,12 +3,12 @@
 export DEBIAN_FRONTEND=noninteractive
 
 # make sure we have all ubuntu repositories
-sudo cp /vagrant/conf/apt.sources.list /etc/apt/sources.list
+sudo cp /vagrant/apt.sources.list /etc/apt/sources.list
 
 
 ## install ubuntu packages
 sudo -E apt-get update -q
-cat /vagrant/conf/debian_packages | xargs sudo -E apt-get install -y -q
+cat /vagrant/debian_packages | xargs sudo -E apt-get install -y -q
 sudo -E apt-get upgrade -y -q
 
 # add the SKA-SA launchpad PPA
@@ -18,25 +18,12 @@ sudo -E apt-get update -q
 # install all python modules
 sudo rm -rf /tmp/pip_build_root/*  ## cleanup any failed builds
 sudo pip install astropy ## pip dependency management doesn't work properly
-sudo pip install -r /vagrant/conf/python_packages
+sudo pip install -r /vagrant/python_packages
 
-###
-###
-### to install the stable packages
-###
-###
 cat /vagrant/conf/ska_packages | xargs sudo -E apt-get install -y -q
 
-##
-##
-## OR to build everything from source
-##
-##
-#/vagrant/scripts/build.sh
-
-
 # configure a ipython noteboot service
-sudo cp /vagrant/conf/supervisord-vagrant.conf /etc/supervisor/conf.d/ipython.conf
+sudo cp /vagrant/supervisord.conf /etc/supervisor/conf.d/ipython.conf
 sudo service supervisor stop
 sudo service supervisor start
 
